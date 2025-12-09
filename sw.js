306ff70f258af9b93f948b8bf164e1deab08a937
nilaoutbound <nilaoutbound@gmail.com>
2025-12-09 22:42:03 +0700

    Refactor event listeners in service worker

--- sw.js
+++ sw.js
@@ -1,3 +1,11 @@
-self.addEventListener('install', e => {
+self.addEventListener('install', event => {
   self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  // Bisa tambahkan caching nanti jika perlu
 });
